a1b592bebcc8ac2208bb5d21330a614755932c93
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2016-09-24 20:10:57 -0300

    best config, but bad page

--- reporter.py
+++ reporter.py
@@ -1,9 +1,22 @@
-from re import compile, MULTILINE, DOTALL
 from sys import argv
-from pandas import DataFrame as df
-from bokeh.charts import Donut, show, output_file
+from re import compile as r_compile
+from re import MULTILINE, DOTALL
+from bokeh.charts import Donut
 from bokeh.embed import file_html
 from bokeh.resources import CDN
+from pandas import DataFrame as df
+from pandas import concat
+
+#Regex
+REMOVE_SCENARIOS = r_compile(
+    r"@scenario.begin(.*?)@scenario.end", MULTILINE | DOTALL)
+R_SCENARIOS = r_compile(r"Cenário: .*|Contexto: .*")
+R_STEPS = r_compile(
+    r"\s+(Então|Quando|Dado)(.*?) ... (failed|passed|skipped) in (.*)")
+
+L_DFS = [] # ----- Lista dos dataframes para compilação
+
+FILO = []   # ----- Fila para o html
 
 def head(file):
     """
@@ -17,24 +30,47 @@ def head(file):
     file.write('<div align="center">\n')
     file.write('<h1>BEHAVE REPORT</h1>\n')
 
-remove_scenarios = compile(
-    "@scenario.begin(.*?)@scenario.end", MULTILINE | DOTALL)
-scenarios = compile("Cenário: .*|Contexto: .*")
-steps = compile("\s+(Então|Quando|Dado)(.*?) ... (failed|passed|skipped) in (.*)")
 
-xml = open(argv[1]).read()
-html = open(argv[2], 'w')
-
-a = remove_scenarios.findall(xml)[0].lstrip()
-b = scenarios.findall(a)[0]
-c = steps.findall(a)
-
-g = df(c, columns=['step', 'text', 'state', 'time'])
+def mount_graph(file):
+    """
+    Função que monta o gráfico usando os dataframes
+    """
+    all_df = concat(L_DFS)
+    graph = Donut(all_df, label='state')
+    file.write(file_html(graph, CDN))
 
 
-h = Donut(g, label='state')
+def mount_page(file):
+    """
+    Esgota a fila enviado para o html
+    """
+    for saida in FILO:
+        file.write(saida)
 
-head(html)
-html.write(file_html(h, CDN))
-html.write("<h4>{}</h4>".format(b))
-html.write(g.to_html())
+
+def todo(file):
+    """
+    função que precisa ser modificada
+    pois faz tudo
+    """
+    for text in REMOVE_SCENARIOS.findall(file):
+        splited_text = text.lstrip()
+
+        steps = R_STEPS.findall(splited_text)
+
+        text_df = df(steps, columns=['step', 'text', 'state', 'time'])
+        L_DFS.append(text_df)
+
+        FILO.append("<h4>{}</h4>".format("".join(R_SCENARIOS.findall(splited_text))))
+        FILO.append(text_df.to_html())
+
+if __name__ == '__main__':
+    XML = open(argv[1]).read()
+    HTML = open(argv[2], 'w')
+
+    head(HTML)
+    todo(XML)
+    mount_graph(HTML)
+    mount_page(HTML)
+
+    HTML.close()
