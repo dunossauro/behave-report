3d158e16d31c3ef1dc2f37445eaa3e4f1e0048fe
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2016-09-25 12:44:40 -0300

    bokeh components; style table

--- reporter.py
+++ reporter.py
@@ -2,8 +2,8 @@ from sys import argv
 from re import compile as r_compile
 from re import MULTILINE, DOTALL
 from bokeh.charts import Donut
-from bokeh.embed import file_html
-from bokeh.resources import CDN
+from bokeh.embed import components
+from bokeh.resources import INLINE
 from pandas import DataFrame as df
 from pandas import concat
 
@@ -18,37 +18,61 @@ L_DFS = [] # ----- Lista dos dataframes para compilação
 
 FILO = []   # ----- Fila para o html
 
-def head(file):
+def head(file, tup):
     """
     Cria o head de do arquivo do parametro
     """
+    style_table = """
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+            }
+
+        tr:nth-child(even){background-color: #f2f2f2}
+    </style>
+    """
+    file.write('<!DOCTYPE html>\n')
+    file.write('<html>\n')
     file.write('<head>\n')
-    file.write('<meta charset="utf-8" />\n')
-    file.write('<title>Behave Report</title>\n')
+    file.write('\t<meta charset="utf-8" />\n')
+    file.write('\t<title>Behave Report</title>\n')
+    file.write(INLINE.render_js())
+    file.write(INLINE.render_css())
+    file.write('{}\n'.format(style_table))
+    file.write('\t{}\n'.format(tup[0]))
     file.write('</head>\n')
     file.write('<body>\n')
-    file.write('<div align="center">\n')
-    file.write('<h1>BEHAVE REPORT</h1>\n')
+    file.write('\t<div align="center">\n')
+    file.write('\t\t<h1>BEHAVE REPORT</h1>\n')
+    file.write('\t{}\n'.format(tup[1]))
+    file.write('\t</div>\n')
 
 
-def mount_graph(file):
+def mount_graph():
     """
     Função que monta o gráfico usando os dataframes
     """
     all_df = concat(L_DFS)
     graph = Donut(all_df, label='state')
-    file.write(file_html(graph, CDN))
+    return components(graph, INLINE)
 
-
-def mount_page(file):
+def mount_page(file, component):
     """
     Esgota a fila enviado para o html
     """
+    head(HTML, component)
     for saida in FILO:
         file.write(saida)
+    file.write('</html>')
+    file.close()
 
-
-def todo(file):
+def parse_xml(file):
     """
     Função que itera a regex, cria data frames
     e gera a fila de htmls para o arquivo
@@ -68,9 +92,6 @@ if __name__ == '__main__':
     XML = open(argv[1]).read()
     HTML = open(argv[2], 'w')
 
-    head(HTML)
-    todo(XML)
-    mount_graph(HTML)
-    mount_page(HTML)
-
-    HTML.close()
+    parse_xml(XML)
+    BCOMPS = mount_graph()
+    mount_page(HTML, BCOMPS)
