83bb6bd56b7edfae549acc130163862b403f4b42
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2016-09-25 22:13:21 -0300

    multline strings

--- reporter.py
+++ reporter.py
@@ -54,8 +54,15 @@ def head(file, tup):
     file.write(INLINE.render_js())
     file.write(INLINE.render_css())
     # ---- bootstrap
-    file.write('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">')
-    #file.write('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>')
+    file.write('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"\
+                rel="stylesheet"\
+                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"\
+                crossorigin="anonymous">')
+
+    # file.write('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"\
+    #             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"\
+    #             crossorigin="anonymous">\
+    #             </script>')
 
     file.write('{}\n'.format(style_table))
     file.write('\t{}\n'.format(tup[0]))
