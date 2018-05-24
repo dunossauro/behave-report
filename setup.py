9f9dbb379ddf33206a377a1e86b0409cdd947916
Eduardo Mendes <mendesxeduardo@gmail.com>
2018-05-24 14:17:00 -0300

    tox started work

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='behave-report',
+      version='0.2',
+      description='simple parser to behave xml output',
+      url='https://github.com/dunosauro/behave-report',
+      author='Eduardo Mendes',
+      author_email='mendesxeduardo@gmail.com',
+      license='MIT',
+      packages=['.'],
+      zip_safe=False)
